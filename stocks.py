145f2f134de8b9deea6bf32b271ea09a3149d202
DienHoNguyen91 <110595893+DienHoNguyen@users.noreply.github.com>
2023-07-23 23:59:38 +0700

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,53 +1,66 @@
 import time
 import random
 
-# initialize the prices
-gold = 200.5
-silver = 24.6
-apple = 191.65
-boeing = 211.2
-daimlerag = 71.65
-generalelectric = 110.2
-shell = 2416.81
-unitedairlines = 57.61
-nike = 109.06
+prices = {
+    "gold": 200.5,
+    "silver": 24.6,
+    "apple": 191.65,
+    "boeing": 211.2,
+    "daimlerag": 71.65,
+    "generalelectric": 110.2,
+    "shell": 2416.81,
+    "unitedairlines": 57.61,
+    "nike": 109.06
+}
+
+balance = 1000
+
+def calculate_net_worth():
+    net_worth = balance
+    for stock, price in prices.items():
+        net_worth += price * holdings.get(stock, 0)
+    return net_worth
+
+holdings = {}
 
 while True:
-    # generate random numbers between -1000 and 1000 for each price
-    gold_change = random.uniform(-10, 10)
-    silver_change = random.uniform(-10, 10)
-    apple_change = random.uniform(-10, 10)
-    boeing_change = random.uniform(-10, 10)
-    daimlerag_change = random.uniform(-10, 10)
-    generalelectric_change = random.uniform(-10, 10)
-    shell_change = random.uniform(-10, 10)
-    unitedairlines_change = random.uniform(-10, 10)
-    nike_change = random.uniform(-10, 10)
-
-    # update the prices
-    gold += gold_change
-    silver += silver_change
-    apple += apple_change
-    boeing += boeing_change
-    daimlerag += daimlerag_change
-    generalelectric += generalelectric_change
-    shell += shell_change
-    unitedairlines += unitedairlines_change
-    nike += nike_change
-
-    # make sure the prices are never below 0
-    gold = max(gold, 0)
-    silver = max(silver, 0)
-    apple = max(apple, 0)
-    boeing = max(boeing, 0)
-    daimlerag = max(daimlerag, 0)
-    generalelectric = max(generalelectric, 0)
-    shell = max(shell, 0)
-    unitedairlines = max(unitedairlines, 0)
-    nike = max(nike, 0)
-
-    # print the prices
-    print(f"Gold: {gold:.2f}\nSilver: {silver:.2f}\nApple: {apple:.2f}\nBoeing: {boeing:.2f}\nDaimler AG: {daimlerag:.2f}\nGeneral Electric: {generalelectric:.2f}\nShell: {shell:.2f}\nUnited Airlines: {unitedairlines:.2f}\nNike: {nike:.2f}\n")
-
-    # wait for 10 seconds
+    for stock in prices:
+        prices[stock] += random.uniform(-10, 10)
+        prices[stock] = max(prices[stock], 0)
+    
+    print(f"Current Prices:\nGold: {prices['gold']:.2f}\nSilver: {prices['silver']:.2f}\nApple: {prices['apple']:.2f}\nBoeing: {prices['boeing']:.2f}\nDaimler AG: {prices['daimlerag']:.2f}\nGeneral Electric: {prices['generalelectric']:.2f}\nShell: {prices['shell']:.2f}\nUnited Airlines: {prices['unitedairlines']:.2f}\nNike: {prices['nike']:.2f}\n")
+    net_worth = calculate_net_worth()
+    print(f"Your Balance: {balance:.2f}\nYour Holdings: {holdings}\nYour Net Worth: {net_worth:.2f}\n")
+    
+    action = input("Do you want to buy or sell a stock? ")
+    if action.lower() == "buy":
+        stock = input("Which stock do you want to buy? ")
+        if stock in prices:
+            price = prices[stock]
+            max_shares = int(balance / price)
+            shares = int(input(f"How many shares do you want to buy? (max {max_shares}) "))
+            shares = min(shares, max_shares)
+            cost = shares * price
+            balance -= cost
+            holdings[stock] = holdings.get(stock, 0) + shares
+            print(f"Bought {shares} shares of {stock} for {cost:.2f}.")
+        else:
+            print("Invalid stock.")
+    elif action.lower() == "sell":
+        stock = input("Which stock do you want to sell? ")
+        if stock in holdings:
+            price = prices[stock]
+            shares = int(input(f"How many shares do you want to sell? (max {holdings[stock]}) "))
+            shares = min(shares, holdings[stock])
+            earnings = shares * price
+            balance += earnings
+            holdings[stock] -= shares
+            if holdings[stock] == 0:
+                del holdings[stock]
+            print(f"Sold {shares} shares of {stock} for {earnings:.2f}.")
+        else:
+            print("You don't own any shares of that stock.")
+    else:
+        print("Invalid action.")
+    
     time.sleep(10)
