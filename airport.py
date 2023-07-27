577f4b631698241cae871ee8e8284df3d155aae3
DienHoNguyen91 <110595893+DienHoNguyen@users.noreply.github.com>
2023-07-27 23:56:47 +0700

    Update airport.py

--- airport.py
+++ airport.py
@@ -1,24 +1,15 @@
 import random
 import time
 
-time = '00:00'
+time_str = '00:00'
 airlines = ['Lufthansa', 'British Airways', 'Ryanair', 'Easyjet']
 flightboard = []
 
 for i in range(10):
     airline = random.choice(airlines)
-    flightnumber = random.randint(1000, 9999)
-    departure_time = time.strftime(*%H:%M',time.localtime(random.randint(36000, 54000)))
-
-
-
-    arrival_time = time.strftime(*%H:%М',time.localtime(random.randint(57600, 72000)))
-
-
-
+    flight_number = random.randint(1000, 9999)
+    departure_time = time.strftime('%H:%M', time.localtime(random.randint(36000, 54000)))
+    arrival_time = time.strftime('%H:%M', time.localtime(random.randint(57600, 72000)))
     status = random.choice(['On time', 'Delayed', 'Cancelled'])
-
-
-
     flightboard.append([airline, flight_number, departure_time, arrival_time, status])
-
+    print(flightboard)
