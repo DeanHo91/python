223096cebf5a8a4dbf544daf8178afba90832eed
DienHoNguyen91 <110595893+DienHoNguyen@users.noreply.github.com>
2023-08-11 15:51:34 +0700

    Update monopoly.py

--- monopoly.py
+++ monopoly.py
@@ -1,2 +1,31 @@
-print("wip")
-print("wi[p"))0aa
+#BIGGEST NUMBER: 18
+
+#Special
+go = 0
+CommunityChest = 2, 17
+Chance = 7
+Jail_JustVisiting = 10
+
+#Property
+MediterraneanAvenue = 1
+BalticAvenue = 3
+IncomeTax = 4
+OrientalAvenue = 6
+VermontAvenue = 8
+ConnecticutAvenue = 9
+StCharlesPlace = 11
+StatesAvenue = 13
+VirginiaAvenue = 14
+StJamesPlace = 16
+TennesseeAvenue = 18
+
+
+#Railroad/Ultilities
+ReadingRailroad = 5
+ElectricCompany = 12
+PennsylvaniaRailroad = 15
+
+#Property color
+brown = [MediterraneanAvenue, BalticAvenue]
+light_blue = [OrientalAvenue, VermontAvenue, ConnecticutAvenue]
+pink = [StCharlesPlace, StatesAvenue, VirginiaAvenue]
