0a82f89dd08698dcedd5fad8d2584d19a1be5241
DienHoNguyen91 <110595893+DienHoNguyen@users.noreply.github.com>
2023-08-25 16:32:29 +0700

    Update monopoly.py

--- monopoly.py
+++ monopoly.py
@@ -1,68 +1,197 @@
 import random
 
-#Special
-go = 0
-CommunityChest = [2, 17, 33]
-Chance = [7, 22, 36]
-Jail_JustVisiting = 10
-FreeParking = 20
-GoToJail = 30
+# Define the properties and their positions
+properties = {
+    0: 'Start',
+    1: 'Mediterranean Avenue',
+    2: 'Community Chest',
+    3: 'Baltic Avenue',
+    4: 'Income Tax',
+    5: 'Reading Railroad',
+    6: 'Oriental Avenue',
+    7: 'Chance',
+    8: 'Vermont Avenue',
+    9: 'Connecticut Avenue',
+    10: 'Jail/Just Visiting',
+    11: 'St. Charles Place',
+    12: 'Electric Company',
+    13: 'States Avenue',
+    14: 'Virginia Avenue',
+    15: 'Pennsylvania Railroad',
+    16: 'St. James Place',
+    17: 'Community Chest',
+    18: 'Tennessee Avenue',
+    19: 'New York Avenue',
+    20: 'Free Parking',
+    21: 'Kentucky Avenue',
+    22: 'Chance',
+    23: 'Indiana Avenue',
+    24: 'Illinois Avenue',
+    25: 'B&O Railroad',
+    26: 'Atlantic Avenue',
+    27: 'Ventnor Avenue',
+    28: 'Water Works',
+    29: 'Marvin Gardens',
+    30: 'Go To Jail',
+    31: 'Pacific Avenue',
+    32: 'North Carolina Avenue',
+    33: 'Community Chest',
+    34: 'Pennsylvania Avenue',
+    35: 'Short Line',
+    36: 'Chance',
+    37: 'Park Place',
+    38: 'Luxury Tax',
+    39: 'Boardwalk'
+}
 
-#Property
-MediterraneanAvenue = 1
-BalticAvenue = 3
-OrientalAvenue = 6
-VermontAvenue = 8
-ConnecticutAvenue = 9
-StCharlesPlace = 11
-StatesAvenue = 13
-VirginiaAvenue = 14
-StJamesPlace = 16
-TennesseeAvenue = 18
-NewYorkAvenue = 19
-KentuckyAvenue = 21
-IndianaAvenue = 23
-IllinoisAvenue = 24
-AtlanticAvenue = 26
-VentnorAvenue = 27
-MarvinGardens = 29
-PacificAvenue = 31
-NorthCarolinaAvenue = 32
-PennsylvaniaAvenue = 34
-ParkPlace = 37
-BoardWalk = 39
+# Define the colors of the properties
+colors = {
+    'brown': [1, 3],
+    'light blue': [6, 8, 9],
+    'pink': [11, 13, 14],
+    'orange': [16, 18, 19],
+    'red': [21, 23, 24],
+    'yellow': [26, 27, 29],
+    'green': [31, 32, 34],
+    'dark blue': [37, 39]
+}
 
-#Railroad/Ultilities
-ReadingRailroad = 5
-ElectricCompany = 12
-PennsylvaniaRailroad = 15
-BORailroad = 25
-WaterWorks = 28
-ShortLine = 35
+# Define the prices of the properties
+prices = {
+    1: 60,
+    3: 60,
+    5: 200,
+    6: 100,
+    8: 100,
+    9: 120,
+    11: 140,
+    12: 150,
+    13: 140,
+    14: 160,
+    15: 200,
+    16: 180,
+    18: 180,
+    19: 200,
+    21: 220,
+    23: 220,
+    24: 240,
+    25: 200,
+    26: 260,
+    27: 260,
+    28: 150,
+    29: 280,
+    31: 300,
+    32: 300,
+    34: 320,
+    35: 200,
+    37: 350,
+    39: 400
+}
 
-#Tax
-IncomeTax = 4
-LuxuryTax = 38
+# Define the player's starting money
+money = 1500
 
-#Property color
-brown = [MediterraneanAvenue, BalticAvenue]
-light_blue = [OrientalAvenue, VermontAvenue, ConnecticutAvenue]
-pink = [StCharlesPlace, StatesAvenue, VirginiaAvenue]
-orange = [StJamesPlace, TennesseeAvenue, NewYorkAvenue]
-red = [KentuckyAvenue, IndianaAvenue, IllinoisAvenue]
-yellow = [AtlanticAvenue, VentnorAvenue, MarvinGardens]
-green = [PacificAvenue, NorthCarolinaAvenue, PennsylvaniaAvenue]
-dark_blue = [ParkPlace, BoardWalk]
+# Define the player's position
+position = 0
 
-#Mechanics
-dice1 = random.randint(1, 6)
-dice2 = random.randint(1, 6)
-move = 0
-ai1dice1 = random.randint(1, 6)
-ai1dice2 = random.randint(1, 6)
-ai1move = 0
-ai2dice1 = random.randint(1, 6)
-ai2dice2 = random.randint(1, 6)
-ai2move = 0
+# Define a function to roll the dice
+def roll_dice():
+    return random.randint(1, 6) + random.randint(1, 6)
 
-#Summary
+# Define a function to check if a property is owned
+def is_owned(property):
+    return property in owned_properties
+
+# Define a function to buy a property
+def buy_property(property):
+    global money
+    if money >= prices[property]:
+        money -= prices[property]
+        owned_properties.append(property)
+        print(f"You bought {properties[property]} for ${prices[property]}")
+    else:
+        print("You don't have enough money to buy this property")
+
+# Define a function to pay rent on a property
+def pay_rent(property):
+    rent = prices[property] // 10
+    owner = property_owners[property]
+    owner_money = player_money[owner]
+    if owner_money >= rent:
+        player_money[owner] -= rent
+        money += rent
+        print(f"You paid ${rent} in rent to {player_names[owner]}")
+    else:
+        print(f"{player_names[owner]} doesn't have enough money to collect rent")
+
+# Define a function to handle landing on a property
+def handle_property(property):
+    if is_owned(property):
+        if property_owners[property] == player:
+            print("You already own this property")
+        else:
+            pay_rent(property)
+    else:
+        buy_property(property)
+
+# Define the players
+num_players = int(input("How many players? "))
+player_names = []
+player_money = []
+owned_properties = []
+property_owners = {}
+for i in range(num_players):
+    name = input(f"Enter player {i+1}'s name: ")
+    player_names.append(name)
+    player_money.append(money)
+
+# Play the game
+while True:
+    player = position % num_players
+    print(f"{player_names[player]}'s turn")
+    input("Press enter to roll the dice")
+    move = roll_dice()
+    position += move
+    print(f"You rolled {move} and landed on {properties[position]}")
+    if position in [2, 17, 33]:
+        print("Community Chest")
+    elif position in [7, 22, 36]:
+        print("Chance")
+    elif position == 30:
+        print("Go to Jail")
+        position = 10
+    elif position == 4:
+        print("Income Tax")
+        player_money[player] -= 200
+    elif position == 38:
+        print("Luxury Tax")
+        player_money[player] -= 75
+    elif position == 20:
+        print("Free Parking")
+    elif position == 10:
+        print("Jail/Just Visiting")
+    elif position in colors['brown']:
+        handle_property(position)
+    elif position in colors['light blue']:
+        handle_property(position)
+    elif position in colors['pink']:
+        handle_property(position)
+    elif position in colors['orange']:
+        handle_property(position)
+    elif position in colors['red']:
+        handle_property(position)
+    elif position in colors['yellow']:
+        handle_property(position)
+    elif position in colors['green']:
+        handle_property(position)
+    elif position in colors['dark blue']:
+        handle_property(position)
+    elif position in [5, 15, 25, 35]:
+        print("Railroad")
+    elif position in [12, 28]:
+        print("Utility")
+    else:
+        print("Error: Invalid position")
+    print(f"{player_names[player]} has ${player_money[player]}")
+    if input("Continue playing? (y/n) ").lower() == 'n':
+        break
